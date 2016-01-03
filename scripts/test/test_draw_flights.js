480d08ff3d691d4ec3e3467b1beb6d98060bc246
klinvill <kjlinvill@gmail.com>
2016-01-02 18:23:56 -0800

    Added build and test components using npm, karma, and jasmin
    Changed the directory structure to better fit karma's standards. Included RequireJS (should probably be replaced with Browserify at some point in the future). The project needs to be refactored to use modules. Integration with Travis-ci is the next step.

--- scripts/test/test_draw_flights.js
+++ scripts/test/test_draw_flights.js
@@ -1,52 +1,54 @@
 // Requires draw_flights.js
+define(['d3', 'draw_flights'], function(d3, df) {
 
-describe ( "Test draw_flights.js", function() {
+    describe ( "Test draw_flights.js", function() {
 
-    it ("buildFlightCount counts both origin and destination", function (done) {
-        var origin = "ABC";
-        var destination = "XYZ";
-        var airline = "foo"
-        var count = 11
+        it ("buildFlightCount counts both origin and destination", function (done) {
+            var origin = "ABC";
+            var destination = "XYZ";
+            var airline = "foo"
+            var count = 11
 
-        var flight = {
-            "ORIGIN": origin
-            ,"DEST": destination
-            ,"UNIQUE_CARRIER": airline            
-            ,"FLIGHTS": count
-        }
+            var flight = {
+                "ORIGIN": origin
+                ,"DEST": destination
+                ,"UNIQUE_CARRIER": airline            
+                ,"FLIGHTS": count
+            }
 
-        var res = buildFlightCount(flight, new Map());
+            var res = df.buildFlightCount(flight, new Map());
 
-        // Both the origin and destination should have recorded the flights
-        expect(res.get(origin).get(airline)).toEqual(count);
-        expect(res.get(destination).get(airline)).toEqual(count);
+            // Both the origin and destination should have recorded the flights
+            expect(res.get(origin).get(airline)).toEqual(count);
+            expect(res.get(destination).get(airline)).toEqual(count);
 
-        // currently tested as asynchronous to pause long enough for the function to load
-        done();
+            // currently tested as asynchronous to pause long enough for the function to load
+            done();
+        })
+
+        it ("buildFlightCount counts both origin and destination even if they are the same", function (done) {
+            var origin = "ABC";
+            var destination = origin;
+            var airline = "foo"
+            var count = 11
+
+            var flight = {
+                "ORIGIN": origin
+                ,"DEST": destination
+                ,"UNIQUE_CARRIER": airline            
+                ,"FLIGHTS": count
+            }
+
+            var res = df.buildFlightCount(flight, new Map());
+
+            // Both the origin and destination should have recorded the flights
+            expect(res.get(origin).get(airline)).toEqual(2 * count);
+        
+            // currently tested as asynchronous to pause long enough for the function to load
+            done();
+        })
+
+        //it ("buildFlightCount fails for a negative flight count", function() {})
+
     })
-
-    it ("buildFlightCount counts both origin and destination even if they are the same", function (done) {
-        var origin = "ABC";
-        var destination = origin;
-        var airline = "foo"
-        var count = 11
-
-        var flight = {
-            "ORIGIN": origin
-            ,"DEST": destination
-            ,"UNIQUE_CARRIER": airline            
-            ,"FLIGHTS": count
-        }
-
-        var res = buildFlightCount(flight, new Map());
-
-        // Both the origin and destination should have recorded the flights
-        expect(res.get(origin).get(airline)).toEqual(2 * count);
-    
-        // currently tested as asynchronous to pause long enough for the function to load
-        done();
-    })
-
-    //it ("buildFlightCounts fails for a negative flight count", function() {})
-
-})
+});
