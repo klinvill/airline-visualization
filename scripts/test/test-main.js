3d808b6eea7ba82285a19b97a1210f8034599e37
klinvill <kjlinvill@gmail.com>
2016-04-03 14:10:49 -0500

    Fixed karma test runner to run against all the js files in the test directory

--- scripts/test/test-main.js
+++ scripts/test/test-main.js
@@ -1,28 +1,30 @@
-var allTestFiles = [];
-var TEST_REGEXP = /(spec|test)_.*\.js$/i;
+// @deprecated: No longer used since I replaced requireJS with escript2015 import and export functionality using babel and webpack
 
-// Get a list of all the test files to include
-Object.keys(window.__karma__.files).forEach(function(file) {
-  if (TEST_REGEXP.test(file)) {
-    // Normalize paths to RequireJS module names.
-    // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
-    // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\/scripts\/src\/|\.js$/g, '.js');
-    allTestFiles.push(normalizedTestModule);
-  }
-});
+// var allTestFiles = [];
+// var TEST_REGEXP = /(spec|test)_.*\.js$/i;
 
-require.config({
-  // Karma serves files under /base, which is the basePath from your config file
-  baseUrl: '/base/scripts/src',
+// // Get a list of all the test files to include
+// Object.keys(window.__karma__.files).forEach(function(file) {
+//   if (TEST_REGEXP.test(file)) {
+//     // Normalize paths to RequireJS module names.
+//     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
+//     // then do not normalize the paths
+//     var normalizedTestModule = file.replace(/^\/base\/scripts\/src\/|\.js$/g, '.js');
+//     allTestFiles.push(normalizedTestModule);
+//   }
+// });
 
-  paths: {
-    'd3': '../../node_modules/d3/d3'
-  },
+// require.config({
+//   // Karma serves files under /base, which is the basePath from your config file
+//   baseUrl: '/base/scripts/src',
 
-  // dynamically load all test files
-  deps: allTestFiles,
+//   paths: {
+//     'd3': '../../node_modules/d3/d3'
+//   },
 
-  // we have to kickoff jasmine, as it is asynchronous
-  callback: window.__karma__.start
-});
+//   // dynamically load all test files
+//   deps: allTestFiles,
+
+//   // we have to kickoff jasmine, as it is asynchronous
+//   callback: window.__karma__.start
+// });
