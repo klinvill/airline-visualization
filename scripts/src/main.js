245e496e650ed1e4526894cb60fbce5c8762d079
klinvill <kjlinvill@gmail.com>
2016-04-03 20:37:46 -0500

    changed filestructure to a more hierarchical structure as opposed to the previously flat structure

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -11,7 +11,7 @@ import { addFlightCountChart } from "./flight_count.js";
     var geomap = get_geomap();
     drawUS(svg, geomap);
 
-    d3.csv("filtered_airports.csv", function (airport_list) {
+    d3.csv("../data/processed/filtered_airports.csv", function (airport_list) {
         draw_airports(svg, geomap)(airport_list);
 
 
@@ -19,14 +19,14 @@ import { addFlightCountChart } from "./flight_count.js";
         //      flights function is called from within the airport parsing function 
         //      in order to keep this data out of the global scope and to guarantee 
         //      that the data has been loaded.
-        d3.csv("us_flights.csv", function (flights) {
+        d3.csv("../data/processed/us_flights.csv", function (flights) {
             var flightCounts = draw_flights(svg, geomap, airport_list)(flights);
 
             attach_airport_handlers(flightCounts);
         });
     });
 
-    d3.csv("key_airlines.csv", function (airlines) {
+    d3.csv("../data/processed/key_airlines.csv", function (airlines) {
         add_airline_select_box()(airlines);
         attach_airline_handlers();
 
