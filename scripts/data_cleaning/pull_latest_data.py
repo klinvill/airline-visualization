b6452de925d7cd2a09f295a6c28e827941d71bda
klinvill <kjlinvill@gmail.com>
2016-08-02 18:11:28 -0500

    Wrote a few python functions to work with AWS Lambda and S3 to dynamically pull the latest airline data for the visualization

--- scripts/data_cleaning/pull_latest_data.py
+++ scripts/data_cleaning/pull_latest_data.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import boto3
 from os import path
+# from memory_profiler import profile
 
 # Unique Airline Codes for adjustments
 AMERICAN_AIRLINES = "AA"
@@ -98,6 +99,42 @@ def filterRelevantAirportData(airport_csv):
 
 def getFlightData(month, year):
     '''
+    Downloads a slimmed-down csv file of Airline Flight On-Time Performance data from the United States' Bureau of
+    Transportation and Statistics
+    :param month: month to download the data for
+    :param year: year to download the data for
+    :return: contents of flight data csv
+    '''
+    url = "http://www.transtats.bts.gov/DownLoad_Table.asp?Table_ID=236&Has_Group=3&Is_Zipped=0"
+    form = "UserTableName=On_Time_Performance&DBShortName=On_Time&RawDataTable=T_ONTIME&sqlstr=+SELECT+UNIQUE_CARRIER%2CFL_NUM%2CORIGIN%2CDEST%2CFLIGHTS+FROM++T_ONTIME+" \
+           "WHERE+Month+%3D{month}+" \
+           "AND+YEAR%3D{year}" \
+           "&varlist=UNIQUE_CARRIER%2CFL_NUM%2CORIGIN%2CDEST%2CFLIGHTS&grouplist=&suml=&sumRegion=&filter1=title%3D&filter2=title%3D&geo=All%A0&time=May&timename=Month&GEOGRAPHY=All&XYEAR=2016&FREQUENCY=5&VarDesc=Year&VarType=Num&VarDesc=Quarter&VarType=Num&VarDesc=Month&VarType=Num&VarDesc=DayofMonth&VarType=Num&VarDesc=DayOfWeek&VarType=Num&VarDesc=FlightDate&VarType=Char&VarName=UNIQUE_CARRIER&VarDesc=UniqueCarrier&VarType=Char&VarDesc=AirlineID&VarType=Num&VarDesc=Carrier&VarType=Char&VarDesc=TailNum&VarType=Char&VarName=FL_NUM&VarDesc=FlightNum&VarType=Char&VarDesc=OriginAirportID&VarType=Num&VarDesc=OriginAirportSeqID&VarType=Num&VarDesc=OriginCityMarketID&VarType=Num&VarName=ORIGIN&VarDesc=Origin&VarType=Char&VarDesc=OriginCityName&VarType=Char&VarDesc=OriginState&VarType=Char&VarDesc=OriginStateFips&VarType=Char&VarDesc=OriginStateName&VarType=Char&VarDesc=OriginWac&VarType=Num&VarDesc=DestAirportID&VarType=Num&VarDesc=DestAirportSeqID&VarType=Num&VarDesc=DestCityMarketID&VarType=Num&VarName=DEST&VarDesc=Dest&VarType=Char&VarDesc=DestCityName&VarType=Char&VarDesc=DestState&VarType=Char&VarDesc=DestStateFips&VarType=Char&VarDesc=DestStateName&VarType=Char&VarDesc=DestWac&VarType=Num&VarDesc=CRSDepTime&VarType=Char&VarDesc=DepTime&VarType=Char&VarDesc=DepDelay&VarType=Num&VarDesc=DepDelayMinutes&VarType=Num&VarDesc=DepDel15&VarType=Num&VarDesc=DepartureDelayGroups&VarType=Num&VarDesc=DepTimeBlk&VarType=Char&VarDesc=TaxiOut&VarType=Num&VarDesc=WheelsOff&VarType=Char&VarDesc=WheelsOn&VarType=Char&VarDesc=TaxiIn&VarType=Num&VarDesc=CRSArrTime&VarType=Char&VarDesc=ArrTime&VarType=Char&VarDesc=ArrDelay&VarType=Num&VarDesc=ArrDelayMinutes&VarType=Num&VarDesc=ArrDel15&VarType=Num&VarDesc=ArrivalDelayGroups&VarType=Num&VarDesc=ArrTimeBlk&VarType=Char&VarDesc=Cancelled&VarType=Num&VarDesc=CancellationCode&VarType=Char&VarDesc=Diverted&VarType=Num&VarDesc=CRSElapsedTime&VarType=Num&VarDesc=ActualElapsedTime&VarType=Num&VarDesc=AirTime&VarType=Num&VarName=FLIGHTS&VarDesc=Flights&VarType=Num&VarDesc=Distance&VarType=Num&VarDesc=DistanceGroup&VarType=Num&VarDesc=CarrierDelay&VarType=Num&VarDesc=WeatherDelay&VarType=Num&VarDesc=NASDelay&VarType=Num&VarDesc=SecurityDelay&VarType=Num&VarDesc=LateAircraftDelay&VarType=Num&VarDesc=FirstDepTime&VarType=Char&VarDesc=TotalAddGTime&VarType=Num&VarDesc=LongestAddGTime&VarType=Num&VarDesc=DivAirportLandings&VarType=Num&VarDesc=DivReachedDest&VarType=Num&VarDesc=DivActualElapsedTime&VarType=Num&VarDesc=DivArrDelay&VarType=Num&VarDesc=DivDistance&VarType=Num&VarDesc=Div1Airport&VarType=Char&VarDesc=Div1AirportID&VarType=Num&VarDesc=Div1AirportSeqID&VarType=Num&VarDesc=Div1WheelsOn&VarType=Char&VarDesc=Div1TotalGTime&VarType=Num&VarDesc=Div1LongestGTime&VarType=Num&VarDesc=Div1WheelsOff&VarType=Char&VarDesc=Div1TailNum&VarType=Char&VarDesc=Div2Airport&VarType=Char&VarDesc=Div2AirportID&VarType=Num&VarDesc=Div2AirportSeqID&VarType=Num&VarDesc=Div2WheelsOn&VarType=Char&VarDesc=Div2TotalGTime&VarType=Num&VarDesc=Div2LongestGTime&VarType=Num&VarDesc=Div2WheelsOff&VarType=Char&VarDesc=Div2TailNum&VarType=Char&VarDesc=Div3Airport&VarType=Char&VarDesc=Div3AirportID&VarType=Num&VarDesc=Div3AirportSeqID&VarType=Num&VarDesc=Div3WheelsOn&VarType=Char&VarDesc=Div3TotalGTime&VarType=Num&VarDesc=Div3LongestGTime&VarType=Num&VarDesc=Div3WheelsOff&VarType=Char&VarDesc=Div3TailNum&VarType=Char&VarDesc=Div4Airport&VarType=Char&VarDesc=Div4AirportID&VarType=Num&VarDesc=Div4AirportSeqID&VarType=Num&VarDesc=Div4WheelsOn&VarType=Char&VarDesc=Div4TotalGTime&VarType=Num&VarDesc=Div4LongestGTime&VarType=Num&VarDesc=Div4WheelsOff&VarType=Char&VarDesc=Div4TailNum&VarType=Char&VarDesc=Div5Airport&VarType=Char&VarDesc=Div5AirportID&VarType=Num&VarDesc=Div5AirportSeqID&VarType=Num&VarDesc=Div5WheelsOn&VarType=Char&VarDesc=Div5TotalGTime&VarType=Num&VarDesc=Div5LongestGTime&VarType=Num&VarDesc=Div5WheelsOff&VarType=Char&VarDesc=Div5TailNum&VarType=Char" \
+        .format(year=year, month=month)
+    header = {"Content-Type": "application/x-www-form-urlencoded"}
+
+    response = requests.post(url, form, headers=header)
+
+    if response.status_code != 200:
+        response.raise_for_status()
+
+    # must be a file-like object in order to extract it using ZipFile
+    zipped_data = StringIO(response.content)
+    download_location = response.history[0].headers['location']
+
+    # extract filename from url
+    zip_filename = download_location.split('/')[-1]
+
+    # replace .zip with .csv
+    csv_filename = ".".join(zip_filename.split('.')[:-1]) + ".csv"
+
+    csv_data = extractFromZip(zipped_data, csv_filename)
+
+    return csv_data
+
+
+def getFlightDataComplete(month, year):
+    '''
     Downloads and extracts a csv file of Airline Flight On-Time Performance data from a zipped file from the United
     States' Bureau of Transportation and Statistics
     :param month: month to download the data for
@@ -173,14 +210,14 @@ def adjustSkyWest(flight_number):
 
 
 def mapCarrier(row):
-    if row['UniqueCarrier'] == EXPRESS_JET_AIRLINES:
-        return adjustExpressJet(row['FlightNum'])
+    if row['UNIQUE_CARRIER'] == EXPRESS_JET_AIRLINES:
+        return adjustExpressJet(row['FL_NUM'])
 
-    elif row['UniqueCarrier'] == SKY_WEST_AIRLINES:
-        return adjustSkyWest(row['FlightNum'])
+    elif row['UNIQUE_CARRIER'] == SKY_WEST_AIRLINES:
+        return adjustSkyWest(row['FL_NUM'])
 
     else:
-        return row['UniqueCarrier']
+        return row['UNIQUE_CARRIER']
 
 
 def adjustCarriers(flight_data):
@@ -189,10 +226,10 @@ def adjustCarriers(flight_data):
     column. Done using the information provided by fivethirtyeight
     (http://fivethirtyeight.com/features/how-we-found-the-fastest-flights/#fn-5)
 
-    :param flight_data: pandas dataframe of flight data, must have UniqueCarrier and FlightNum columns
-    :return: pandas dataframe with updated UniqueCarrier values
+    :param flight_data: pandas dataframe of flight data, must have UNIQUE_CARRIER and FlightNum columns
+    :return: pandas dataframe with updated UNIQUE_CARRIER values
     '''
-    flight_data['UniqueCarrier'] = flight_data[['UniqueCarrier', 'FlightNum']].apply(mapCarrier, axis=1)
+    flight_data['UNIQUE_CARRIER'] = flight_data[['UNIQUE_CARRIER', 'FL_NUM']].apply(mapCarrier, axis=1)
     return flight_data
 
 
@@ -203,7 +240,7 @@ def filterRelevantFlightData(flight_data):
     :return: pandas dataframe of slimmed down flight data
     '''
 
-    return flight_data[['Origin', 'Dest', 'UniqueCarrier']]
+    return flight_data[['ORIGIN', 'DEST', 'UNIQUE_CARRIER']]
 
 
 def aggregateFlights(flight_data):
@@ -212,7 +249,7 @@ def aggregateFlights(flight_data):
     :param flight_data: pandas dataframe of flights
     :return:
     '''
-    counts = flight_data.groupby(['Origin', 'Dest', 'UniqueCarrier']).size()
+    counts = flight_data.groupby(['ORIGIN', 'DEST', 'UNIQUE_CARRIER']).size()
 
     # size produces a series where the groupby columns are indices, but we want a flat dataframe
     counts_df = pd.DataFrame(counts, columns=['Flights'])
@@ -231,8 +268,8 @@ def filterActiveAirports(flight_data, airport_data):
     :return: pandas dataframe of enriched active airports
     '''
 
-    origin_airports = flight_data[['Origin', 'Flights']].rename(columns = {'Origin': 'AIRPORT'})
-    dest_airports = flight_data[['Dest', 'Flights']].rename(columns = {'Dest': 'AIRPORT'})
+    origin_airports = flight_data[['ORIGIN', 'Flights']].rename(columns = {'ORIGIN': 'AIRPORT'})
+    dest_airports = flight_data[['DEST', 'Flights']].rename(columns = {'DEST': 'AIRPORT'})
     active_airports = pd.concat([origin_airports, dest_airports])\
         .groupby(['AIRPORT']).aggregate(np.sum)
 
@@ -260,50 +297,87 @@ def denormalizeFlightCarriers(flight_data, airline_data):
     Joins the unique carrier lookup data to the flight data
     :return: pandas dataframe of denormalized flight data
     '''
-    denormalized_flights = pd.merge(flight_data, airline_data, left_on='UniqueCarrier', right_on='Code')\
+    denormalized_flights = pd.merge(flight_data, airline_data, left_on='UNIQUE_CARRIER', right_on='Code')\
         .drop('Code', 1)
     return denormalized_flights
 
 
-if __name__ == "__main__":
-
+# Function called by AWS lambda
+def pullLatestData(event, context):
     # Airline datasets are two months old
     latest_airline_date = date.today() - relativedelta(months=2)
 
     month = latest_airline_date.month
     year = latest_airline_date.year
 
+    if context is not None:
+        print("Starting to pull latest data. Time remaining (MS):", context.get_remaining_time_in_millis())
+
     flight_csv = StringIO(getFlightData(month, year))
     flight_data = pd.read_csv(flight_csv)
+
+    if context is not None:
+        print("Flight data read. Time remaining (MS):", context.get_remaining_time_in_millis())
+
     flight_data = adjustCarriers(flight_data)
     flight_data = filterRelevantFlightData(flight_data)
     flight_data = aggregateFlights(flight_data)
 
+    if context is not None:
+        print("Flight data processed. Time remaining (MS):", context.get_remaining_time_in_millis())
+
     airport_csv = StringIO(getAirportData())
+
+    if context is not None:
+        print("Airport data read. Time remaining (MS):", context.get_remaining_time_in_millis())
+
     airport_data = filterRelevantAirportData(airport_csv)
     active_airports = filterActiveAirports(flight_data, airport_data)
+
+    if context is not None:
+        print("Airport data processed. Time remaining (MS):", context.get_remaining_time_in_millis())
 
     airline_csv = StringIO(getAirlineCodes())
     airline_data = pd.read_csv(airline_csv)
 
+    if context is not None:
+        print("Airline data read. Time remaining (MS):", context.get_remaining_time_in_millis())
+
     flight_data = denormalizeFlightCarriers(flight_data, airline_data)
 
-    airline_lookup = flight_data[['UniqueCarrier', 'Description']].drop_duplicates()
+    airline_lookup = flight_data[['UNIQUE_CARRIER', 'Description']].drop_duplicates()
+
+    if context is not None:
+        print("Saving data to s3. Time remaining (MS):", context.get_remaining_time_in_millis())
 
     # Save data as csv files to s3
     s3 = boto3.resource('s3')
 
-    s3.Object(S3_BUCKET, path.join(DATA_DIRECTORY, 'airports.csv'))\
+    s3.Object(S3_BUCKET, path.join(DATA_DIRECTORY, 'airports.csv')) \
         .put(Body=active_airports.to_csv(index=False),
              ACL='public-read')
+
+    if context is not None:
+        print("Airport data written to s3. Time remaining (MS):", context.get_remaining_time_in_millis())
 
     s3.Object(S3_BUCKET, path.join(DATA_DIRECTORY, 'flights.csv')) \
         .put(Body=flight_data.to_csv(index=False),
              ACL='public-read')
 
+    if context is not None:
+        print("Flight data written to s3. Time remaining (MS):", context.get_remaining_time_in_millis())
+
     s3.Object(S3_BUCKET, path.join(DATA_DIRECTORY, 'airlines.csv')) \
         .put(Body=airline_lookup.to_csv(index=False),
              ACL='public-read')
+
+    if context is not None:
+        print("Airline data written to s3. Time remaining (MS):", context.get_remaining_time_in_millis())
+
+
+if __name__ == "__main__":
+    pullLatestData(None, None)
+
 
 
 
